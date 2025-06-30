ae88962855c3ca825d0301eaf788b3316b090620
fedinairn08 <fedina_irn08@mail.ru>
2025-06-30 14:50:48 +0300

    Создан проект

---
+++ src/main/java/com/viewTrack/ViewTrackApplication.java
@@ -0,0 +1,13 @@
+package com.viewTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viewTrack/ViewTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viewTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
